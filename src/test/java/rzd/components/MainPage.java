636f9812213afa30dfa4a9a33fc0aa17e731359e
Artemy-jzs-161 <borovik.artemy@yandex.ru>
2024-04-10 02:15:26 +0700

    refactor

--- src/test/java/rzd/components/MainPage.java
+++ src/test/java/rzd/components/MainPage.java
@@ -2,15 +2,11 @@ package rzd.components;
 
 import com.codeborne.selenide.SelenideElement;
 
-import java.util.List;
-
-import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class MainPage {
 
     private final SelenideElement
-            clickCheckLanguage = $(".icon-locale"),
             buttonVersionForVisuallyImpaired = $(".blind__text");
 
     public MainPage openPageForVisuallyImpaired() {
--- src/test/java/rzd/tests/TestBase.java
+++ src/test/java/rzd/tests/TestBase.java
@@ -1,6 +1,8 @@
 package rzd.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import rzd.components.MainPage;
 import rzd.components.RailwayStationPage;
@@ -20,5 +22,10 @@ public class TestBase {
         Configuration.baseUrl = "https://www.rzd.ru";
         Configuration.pageLoadStrategy = "eager";
 
+    }
+
+    @AfterEach
+    void afterEach() {
+        Selenide.closeWebDriver();
     }
 }
